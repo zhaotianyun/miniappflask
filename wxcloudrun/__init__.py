8a2ccef04099def926a013e4e67a234094baf22e
zhaotianyun <ztyx9909@163.com>
2024-11-05 14:47:53 +0800

    optimize code

--- wxcloudrun/__init__.py
+++ wxcloudrun/__init__.py
@@ -6,19 +6,20 @@ import config
 # 因MySQLDB不支持Python3，使用pymysql扩展库代替MySQLDB库
 pymysql.install_as_MySQLdb()
 
-# 初始化web应用
+# 初始化 web 应用
 app = Flask(__name__, instance_relative_config=True)
-app.config['DEBUG'] = config.DEBUG
-
-# 设定数据库链接
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}/flask_demo'.format(config.username, config.password,
-                                                                             config.db_address)
-
-# 初始化DB操作对象
-db = SQLAlchemy(app)
-
-# 加载控制器
-from wxcloudrun import views
 
 # 加载配置
-app.config.from_object('config')
+app.config.from_object(config)
+
+# 设置数据库 URI
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{config.username}:{config.password}@{config.db_address}/flask_demo'
+
+# 关闭 SQLALCHEMY_TRACK_MODIFICATIONS，避免无意义的性能开销
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# 初始化 DB 操作对象
+db = SQLAlchemy(app)
+
+# 加载控制器（视图函数）
+from wxcloudrun import views
